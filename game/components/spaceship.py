7fa1993b9a3d56d3b689c93540363d7ede494283
Ramiro Escobar Villegas <Ramiro.Escobar@fundacion-jala.org>
2023-07-18 23:19:26 -0400

    Second_Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -8,6 +8,7 @@ class Spaceship(Sprite):
     SPACESHIP_HEIGHT = 60
     X_POS = (SCREEN_WIDTH//2) - SPACESHIP_WIDTH//2
     Y_POS = 500
+    SPEED = 10
 
     def __init__(self):
         self.image = pygame.transform.scale(SPACESHIP, (self.SPACESHIP_WIDTH, self.SPACESHIP_HEIGHT))
@@ -17,23 +18,19 @@ class Spaceship(Sprite):
 
     def update(self, user_input):
      if user_input[pygame.K_LEFT]:
-        self.rect.x -= 10
+        self.rect.x -= self.SPEED
         if self.rect.left < 0:
-           self.rect.left = SCREEN_WIDTH - 60
+           self.rect.left = SCREEN_WIDTH - self.SPACESHIP_WIDTH
      elif user_input[pygame.K_RIGHT]:
-        self.rect.x += 10
+        self.rect.x += self.SPEED
         if self.rect.right > SCREEN_WIDTH:
-           self.rect.right = 60          
+           self.rect.right = self.SPACESHIP_WIDTH          
      elif user_input[pygame.K_UP]:
         if self.rect.top > SCREEN_HEIGHT//2:
-            self.rect.y -= 10
+            self.rect.y -= self.SPEED
      elif user_input[pygame.K_DOWN]:
         if self.rect.bottom < SCREEN_HEIGHT:
-           self.rect.bottom += 10
-
-        
-
+           self.rect.bottom += self.SPEED
+       
     def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
-
-         
+        screen.blit(self.image, (self.rect.x, self.rect.y)) 
